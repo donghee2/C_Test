5e3e225390e3563379e496674a751f439e98d082
leedonghee <leedh7785@naver.com>
2022-09-14 15:05:31 +0900

    commit

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 enum {
+    EXIT,
     INSERT,
     SEARCH,
     DELETE,
@@ -23,5 +24,85 @@ void InsertTel(){
     scanf(" %s",arr[idx].name);
     printf("핸드폰번호 입력 : ");
     scanf(" %d",&arr[idx].tel);
-    idx++;//인덱스 번호 하나 증가
+    idx++;
+    printf("등록완료.\n");
 }
+void SearchTel(){
+    char str[30];
+    printf("검색할 이름 입력 : ");
+    scanf("%s",str);
+    for(int i=0;i<idx;i++){
+        if(strcmp(arr[i].name,str) == 0){
+            printf("%s %d %c\n",arr[i].name, arr[i].tel);  
+            return;
+        }
+    }
+    printf("검색 결과가 없습니다.\n");
+}
+void DeleteTel(){
+    char str[30];
+    printf("삭제할 이름 입력 : ");
+    scanf("%s",str);
+    for(int i=0;i<idx;i++){
+        if(strcmp(arr[i].name,str) == 0){
+            for(int j=i;j<idx-1;j++)
+                arr[j] = arr[j+1];
+            idx--;
+            return;
+        }
+    }
+    printf("삭제할 데이터가 없습니다.\n");
+}
+void UpdateTel(){
+    char search[30];
+    int flag = 0;
+    printf("검색할 이름을 입력하세요 : ");
+    scanf(" %s", search );
+    for(int i=0; i<idx;i++){
+        if(strcmp(search, arr[i].name) == 0){
+            printf("수정할 연락처를 입력하세요 : ");
+            scanf(" %s", arr[i].tel);
+            flag = 1;
+        }
+    }
+    if(flag == 1) printf("연락처 수정 완료\n");
+    else printf("검색한 사용자를 찾을 수 없습니다\n");
+
+}
+int main(void){
+    int no = -1;
+
+    while(no != EXIT){
+        puts("--- 연락처 관리 프로그램---");
+        puts("1. 등록");
+        puts("2. 검색");
+        puts("3. 삭제");
+        puts("4. 수정");
+        puts("0. 프로그램 종료");
+        puts("원하시는 메뉴 번호 입력 : ");
+        scanf("%d",&no);
+
+        switch(no){
+            case INSERT:
+                InsertTel();
+                break;
+            case SEARCH:
+                SearchTel();
+                break;
+            case DELETE:
+                DeleteTel();
+                break;
+            case UPDATE:
+                UpdateTel();
+                break;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
+
+
